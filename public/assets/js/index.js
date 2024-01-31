4908aa6bae0a240c5853108bd56949c350b6a900
SimpleSuyash <suyashmaharjan@gmail.com>
2024-02-01 10:42:48 +1100

    get method added

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -183,7 +183,7 @@ const renderNoteList = async (notes) => {
 
 // Gets notes from the db and renders them to the sidebar
 const getAndRenderNotes = () => getNotes().then(renderNoteList);
-
+console.log(window.location.pathname === '/notes');
 if (window.location.pathname === '/notes') {
   saveNoteBtn.addEventListener('click', handleNoteSave);
   newNoteBtn.addEventListener('click', handleNewNoteView);
